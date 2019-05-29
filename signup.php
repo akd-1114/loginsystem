ef6565cee0332984eb6fb42628ae8f3ebe08b054
akd_1114 <04akashdeep@gmail.com>
2019-05-29 12:53:01 +0530

    added some changes

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
 <section class="main-container">
   <div class="main-wrapper">
      <h2>Signup</h2>
-       <form class="signup-form" action="signup.inc.php" method="POST">
+       <form /*class="signup-form"*/ action="signup.inc.php" method="POST">
            <input type="text" name="first" placeholder="firstname">
            <input type="text" name="last" placeholder="lastname">
            <input type="text" name="email" placeholder="e-mail">
