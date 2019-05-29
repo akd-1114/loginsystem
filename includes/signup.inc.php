2a2ab72c814db4b077c73d25f4a58a3bd7e663cc
akd_1114 <04akashdeep@gmail.com>
2019-05-29 12:43:58 +0530

    this is our first commit

---
+++ includes/signup.inc.php
@@ -0,0 +1,56 @@
+<?php
+ if(isset($_POST['submit']))
+ {
+     include_once 'connection.php';
+    $first=mysqli_real_escape_string($conn,$_POST['first']);
+    $last=mysqli_real_escape_string($conn,$_POST['last']);
+    $email=mysqli_real_escape_string($conn,$_POST['email']);
+    $uid=mysqli_real_escape_string($conn,$_POST['uid']);
+    $pwd=mysqli_real_escape_string($conn,$_POST['pwd']);
+    if(empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd))
+    {
+        header("Location: ../signup.php?signup=empty");
+        exit(); 
+    }
+    else
+    {
+       if(!preg_match("/^[a-zA-Z]*$/",$first)||!preg_match("/^[a-zA-Z]*$/",$last))
+       {
+        header("Location: ../signup.php?signup=invalid");
+        exit(); 
+       }
+       else
+       {
+          if(!filter_var($email,FILTER_VALIDATE_EMAIL))
+          {
+            header("Location: ../signup.php?signup=email");
+            exit();
+          }
+          else
+          {
+            $sql="select * fron users where u_id='$uid'";
+            $result=mysqli_query($conn,$sql);
+            $resultcheck=mysqli_num_rows($result);
+            if($resultcheck>0)
+            {
+                header("Location: ../signup.php?signup=usertaken");
+            exit();
+            }
+            else
+            {
+                $hashedpwd=password_hash($pwd,PASSWORD_DEFAULT);
+               $sql="insert into users (u_first,u_last,u_email,u_uid,u_pwd) values ('$first','$last','$email','$uid','$hashedpwd')";
+               mysqli_query($conn,$sql);
+               header("Location: ../signup.php?signup=success");
+               exit();
+            }
+          }
+       }
+    }
+ }
+ else
+ {
+     header("Location: ../signup.php");
+     exit();
+ }
+?>
